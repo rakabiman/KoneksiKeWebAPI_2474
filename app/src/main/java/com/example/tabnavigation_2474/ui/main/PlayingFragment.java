690824d5c4dab071151e54c31a509d93c09afe8c
Rakabiman <zulfikar.raka@gmail.com>
2020-12-07 07:35:36 +0700

    Initial Commit

--- app/src/main/java/com/example/tabnavigation_2474/ui/main/PlayingFragment.java
+++ app/src/main/java/com/example/tabnavigation_2474/ui/main/PlayingFragment.java
@@ -10,38 +10,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.tabnavigation_2474.ApiClient;
+import com.example.tabnavigation_2474.ApiService;
 import com.example.tabnavigation_2474.Movie;
 import com.example.tabnavigation_2474.MovieAdapter;
+import com.example.tabnavigation_2474.MovieResponse;
 import com.example.tabnavigation_2474.R;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PlayingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
 public class PlayingFragment extends Fragment {
 
-    private String[] movie = {"Bad Boys for Life","The Old Guard","Raised by Wolves","Elite","The Walking Dead: World Beyond","Artemis Fowl","Black Box","Riverdale","Law & Order: Special Victims Unit","Scary Movie 5","Star Trek: Discovery","Hubie Halloween","District 9","The Hurricane Heist","Paddington 2","Pride & Prejudice "};
-    private String[] movieYear = {"2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013", "2014","2015","2016"};
     private ArrayList<Movie> listMovie;
     private RecyclerView rvMovie;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_playing, container, false);
-        listMovie = new ArrayList<>();
-        for (int i = 0; i < movie.length ; i++) {
-            Movie mov = new Movie(movie[i],movieYear[i]);
-            listMovie.add(mov);
-        }
         rvMovie = view.findViewById(R.id.rv_movie);
         rvMovie.setHasFixedSize(true);
         rvMovie.setLayoutManager(new LinearLayoutManager(getContext()));
-        MovieAdapter movieAdapter = new MovieAdapter(listMovie);
-        rvMovie.setAdapter(movieAdapter);
+        ApiService service = ApiClient.getRetrofitInstance().create(ApiService.class);
+        Call<MovieResponse> call =
+                service.getPlayingMovie("d58845c9d53da3e216a0a21300e1e90a");
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse>
+                    response) {
+                listMovie = response.body().getResults();
+                MovieAdapter movieAdapter = new MovieAdapter(listMovie);
+                rvMovie.setAdapter(movieAdapter);
+            }
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+            }
+        });
         return view;
     }
 }
